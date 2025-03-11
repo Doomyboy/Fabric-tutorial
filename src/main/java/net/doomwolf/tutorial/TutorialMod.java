dd5e2825a0b46c0db30ef2893d29d0b7e2936908
DoomWolf <Thedoomwolfalpha@gmail.com>
2025-03-10 19:12:53 -0700

    Comment test

---
+++ src/main/java/net/doomwolf/tutorial/TutorialMod.java
@@ -0,0 +1,17 @@
+package net.doomwolf.tutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//test change
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
